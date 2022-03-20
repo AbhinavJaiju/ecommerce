bf3565aa098ab4dfbe4ea1b15953fcf0683575c0
BidhuMRenchi <bidhu.renchi@experionglobal.com>
2022-03-20 20:22:07 +0530

    commi009 by bidhu

--- admin/customer/customer-listing.php
+++ admin/customer/customer-listing.php
@@ -294,7 +294,8 @@
                             <th> PhoneNumber </th>
                             <th> Gender </th>
                             <th> Profile Picture</th>
-                            <th> Action </th>
+                            <th>  </th>
+                            <th></th>
                           </tr>
                         </thead>
                         <tbody>
@@ -318,19 +319,21 @@
                                 <div>
                                     <form action='customer-remove.php?customerId="<?php echo $row['customerId'];?>"'method ="post" >
                                         <input type="hidden" name="customerId" value = "<?php echo $row['customerId'];?>">
-                                        <button type="submit" class="btn btn-danger" onClick="return confirm('Are you Sure?')" name = "delete" >
+                                        <button type="submit" class="btn btn-sm btn-danger" onClick="return confirm('Are you Sure?')" name = "delete" >
                                         <i class="bi bi-trash"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                                                 <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                                                 </svg>
-                                                Delete
+                                                
                                             </i>
                                     </form>
                                 </div>
+                            </td>
+                            <td>
                                 <div >
                                     <a href='customer-edit.php?customerId=<?php echo $row[customerId]?>&fn=<?php echo $row[customerName];?>&em=<?php echo $row[email] ?>&ps=<?php echo $row[passwords]?>&ph=<?php echo $row[phoneNumber]?>&gd=<?php echo $row[gender]?>&ad=<?php echo $row[addresss]?>&im=<?php echo row[profilePicture]?>' >
                                         <input type="hidden" name ="customerId" value = "<?php echo $row['customerId'];?>">
-                                        <button type="submit" class="btn btn-primary" name="edit" onClick = "return Confirm('Are you Sure?')">
+                                        <button type="submit" class="btn btn-sm btn-primary" name="edit" onClick = "return Confirm('Are you Sure?')">
                                         <i class="bi bi-file-earmark-medical">Edit</i>
                                         </button>   
                                 </a>
---
+++ admin/product/category-insertion.php
@@ -0,0 +1,19 @@
+<?php
+    //inluding db connection file 
+    include_once 'config.php';
+
+    $name = $_POST['categoryName'];
+    $description = $_POST['description'];
+
+    //insertion query
+    $sql = "INSERT INTO categories(categoryName,description) 
+            VALUES('{$name}','{$description}')";
+
+    if($conn->query($sql)===TRUE){
+        echo "Hello , Category {$name} is inserted.";
+    }else{
+        echo "Could not save your data";
+    }
+    
+    $conn->close();
+?>
