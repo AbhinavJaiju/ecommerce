5a66944038082a27d2ab3d6a78cd82140219886e
binitha-experionglobal <binitha.s@experionglobal.com>
2022-03-20 15:47:32 +0530

    edits navigation file

--- frontend/Front-endview/shop.php
+++ frontend/Front-endview/shop.php
@@ -1,26 +1,26 @@
 <?php
 session_start();
+
+//logout
+
 if (isset($_POST['but_logout'])) {
     session_destroy();
     header('Location: login.php');
 }
-$_SESSION["CategoryId"] = 3;
 
-//get category id from session
+//get,set values -> from sessions and links
 
 $strValue = $_GET['id'];
 if ($strValue > 0) {
     $categoryId = $strValue;
 } else {
-    $categoryId = $_SESSION["CategoryId"];
+    echo"Server error ! Please try again later";
 }
-
-
 $_SESSION["CategoryId"] = $categoryId;
 
+//configuration
 
 include "config.php";
-
 ?>
 
 <!DOCTYPE html>
@@ -83,77 +83,9 @@ include "config.php";
 
 
     <!-- Header Section Begin -->
-    <header class="header">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-xl-3 col-lg-2">
-                    <div class="header__logo">
-                        <a href="./index.php"><img src="img/logo3.png" alt="" style="height: 35px;width: 100px;"></a>
-                    </div>
-                </div>
-                <div class="col-xl-6 col-lg-7">
-                    <nav class="header__menu">
-                        <ul>
-                            <li class="active"><a href="./index.php">Home</a></li>
-
-                            <li><a href="./shop.php">Shop</a></li>
-                            <li><a href="#">Pages</a>
-                                <ul class="dropdown">
-                                    <li><a href="./product-details.php">Product Details</a></li>
-                                    <li><a href="./shop-cart.php">Shop Cart</a></li>
-                                    <li><a href="./checkout.php">Checkout</a></li>
-
-                                </ul>
-                            </li>
-
-                            <li><a href="./contact.php">Contact</a></li>
-                        </ul>
-                    </nav>
-                </div>
-                <div class="col-lg-3">
-                    <div class="header__right">
-                        <div class="header__right__auth">
-                            <?php
-                            if ($_SESSION['uname']) {
-
-                                // echo '<a style="margin-left:10px;font-size:15px" href="http://localhost/ecommerce/frontend/Front-endview/logout.php"><strong>Logout</strong></a>';
-
-                                echo $_SESSION['uname'];
-                            } else {
-                                echo  '<a href="login.php">Login</a>
-                                <a href="#">Register</a>';
-                            }
-
-                            ?>
-
-                        </div>
-
-                        <ul class="header__right__widget">
-                            <li><span class="icon_search search-switch"></span></li>
-                            <li><a href="wish-list.php"><span class="icon_heart_alt"></span>
-                                    <div class="tip">2</div>
-                                </a></li>
-                            <li><a href="shop-cart.php"><span class="icon_bag_alt"></span>
-                                    <div class="tip">2</div>
-                                </a></li>
-                            <li>
-                                <form method='post' action="" style="margin-left:1%">
-                                    <input type="submit" class="btn btn-default btn-sm" style="font-size:15px;font-weight:bold" value="Logout" name="but_logout">
-                                </form>
-                            </li>
-
-
-                        </ul>
-                    </div>
-
-                </div>
-
-            </div>
-            <div class="canvas__open">
-                <i class="fa fa-bars"></i>
-            </div>
-        </div>
-    </header>
+        <?php
+        include "navigation.php";
+        ?>
     <!-- Header Section End -->
 
     <!-- Breadcrumb Begin -->
@@ -378,24 +310,7 @@ include "config.php";
 
 
 
-                        if (isset($_POST['edit'])) {
-
-
-
-
-                            // echo $did = $_POST['id'];
-                            //echo "inside edit"."$did";
-                            // $_COOKIE['productId'] = $did;
-
-
-                            // $_SESSION['id'] = $_POST['id'];
-                            // $var = $_POST['id'];
-                            // echo "$var";
-
-
-
-                            // header("Location: http://localhost/Projects/productEdit.php?id=" . $var);
-                        }
+                        
                         ?>
 
 
