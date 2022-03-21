a1a1535e2deb1c0abf67510d2e08d4d727191330
AlfinaSulfikarB <alfina.b@experionglobal.com>
2022-03-21 20:50:49 +0530

    Edits banner styles

--- admin/config.php
+++ admin/config.php
@@ -11,18 +11,18 @@
       //   die("Connection failed: " . $conn->connect_error);
       // }
 
-      $servername = "localhost";
-   $username = "abhinav.jaiju";
-   $password = "experion@123";
-   $dbname = "ecommerce";
+//       $servername = "localhost";
+//    $username = "abhinav.jaiju";
+//    $password = "experion@123";
+//    $dbname = "ecommerce";
 
-   //create connection
-   $conn = new mysqli($servername,$username,$password, $dbname);
+//    //create connection
+//    $conn = new mysqli($servername,$username,$password, $dbname);
 
-   //check connection
-   if($conn -> connect_error){
-       die("Connection Failed:" . $conn->connect_error);
-   }
+//    //check connection
+//    if($conn -> connect_error){
+//        die("Connection Failed:" . $conn->connect_error);
+//    }
 
 
 // $servername = "localhost";
@@ -52,15 +52,15 @@
 //   die("Connection failed: " . $conn->connect_error);
 // }
 
-// $servername = "localhost";
-// $username = "alfina";
-// $password = "Alfinamemysql@123";
-// $dbname = "ecommerce";
+$servername = "localhost";
+$username = "alfina";
+$password = "Alfinamemysql@123";
+$dbname = "ecommerce";
 
-// /// Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname); //;
-// // Check connection
-// if ($conn->connect_error) {
-//   die("Connection failed: " . $conn->connect_error);
-// }
+/// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname); //;
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 ?>
