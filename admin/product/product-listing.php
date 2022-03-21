638f3172f55c2772c5c3a51bb6b3de755280a169
AbhinavJaiju <abhinavchalakudi@gmail.com>
2022-03-21 16:45:08 +0530

    final

--- admin/product/product-listing.php
+++ admin/product/product-listing.php
@@ -14,9 +14,11 @@
     <!-- End plugin css for this page -->
     <!-- inject:css -->
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="paging.css">
     <!-- endinject -->
     <link rel="shortcut icon" href="../images/favicon.png" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- tablesorter link -->
 
 
 </head>
@@ -163,7 +165,8 @@
                                 <p class="card-description"> List of Products
                                 </p>
                                 <div class="table-responsive">
-                                    <table class="table table-bordered">
+
+                                    <table class="table table-bordered" id="pager">
                                         <a href="../product/product-form.php">
                                             <button type="button" class="btn btn-outline-primary btn-fw" style="float: right;margin-bottom:6px">Add Product</button>
                                         </a>
@@ -184,12 +187,12 @@
                                         </thead>
                                         <tbody>
                                             <?php
-                                            include_once '../config.php';
+                                            include '../config.php';
                                             $sql = "SELECT * from products ";
-                                // LEFT JOIN productImage on products.productId = productImage.productId 
-                                // UNION 
-                                // SELECT * from products
-                                // RIGHT JOIN productImage on products.productId = productImage.productId ";
+                                            // LEFT JOIN productImage on products.productId = productImage.productId 
+                                            // UNION 
+                                            // SELECT * from products
+                                            // RIGHT JOIN productImage on products.productId = productImage.productId ";
                                             $result = $conn->query($sql);
 
                                             while ($row = $result->fetch_assoc()) {
@@ -204,16 +207,16 @@
                                                     <td> <?php echo $row['shortDescription'] ?> </td>
                                                     <td> <?php echo $row['specification'] ?> </td>
                                                     <td>
-                                                        <?php 
-                                                            $sql1 = "SELECT * FROM productImage  WHERE productId = $id";
-                                                            $result1 = $conn->query($sql1);
-                                                            while($row1 = $result1->fetch_assoc()){
-
-                                                            ?>
-                                                        <img src="../../productImages/<?php echo $row1['fileName'] ?>" alt="">
                                                         <?php
-                                                            }
-                                                            ?>
+                                                        $sql1 = "SELECT * FROM productImage  WHERE productId = $id";
+                                                        $result1 = $conn->query($sql1);
+                                                        while ($row1 = $result1->fetch_assoc()) {
+
+                                                        ?>
+                                                            <img src="../../productImages/<?php echo $row1['fileName'] ?>" alt="">
+                                                        <?php
+                                                        }
+                                                        ?>
                                                     </td>
                                                     <td>
                                                         <div>
@@ -244,6 +247,11 @@
                                             ?>
                                         </tbody>
                                     </table>
+                                    <!-- Pagination nav -->
+                                    <div id="pageNavPosition" class="pager-nav">
+                                    </div>
+                                    <!-- pagination nav done-->
+                                
                                 </div>
                             </div>
                         </div>
@@ -272,7 +280,115 @@
         <!-- page-body-wrapper ends -->
     </div>
     <!-- container-scroller -->
+    <!-- pagination function -->
+    <script>
+        /* eslint-env browser */
+        /* global document */
 
+        function Pager(tableName, itemsPerPage) {
+            'use strict';
+
+            this.tableName = tableName;
+            this.itemsPerPage = itemsPerPage;
+            this.currentPage = 1;
+            this.pages = 0;
+            this.inited = false;
+
+            this.showRecords = function(from, to) {
+                let rows = document.getElementById(tableName).rows;
+
+                // i starts from 1 to skip table header row
+                for (let i = 1; i < rows.length; i++) {
+                    if (i < from || i > to) {
+                        rows[i].style.display = 'none';
+                    } else {
+                        rows[i].style.display = '';
+                    }
+                }
+            };
+
+            this.showPage = function(pageNumber) {
+                if (!this.inited) {
+                    // Not initialized
+                    return;
+                }
+
+                let oldPageAnchor = document.getElementById('pg' + this.currentPage);
+                oldPageAnchor.className = 'pg-normal';
+
+                this.currentPage = pageNumber;
+                let newPageAnchor = document.getElementById('pg' + this.currentPage);
+                newPageAnchor.className = 'pg-selected';
+
+                let from = (pageNumber - 1) * itemsPerPage + 1;
+                let to = from + itemsPerPage - 1;
+                this.showRecords(from, to);
+
+                let pgNext = document.querySelector('.pg-next'),
+                    pgPrev = document.querySelector('.pg-prev');
+
+                if (this.currentPage == this.pages) {
+                    pgNext.style.display = 'none';
+                } else {
+                    pgNext.style.display = '';
+                }
+
+                if (this.currentPage === 1) {
+                    pgPrev.style.display = 'none';
+                } else {
+                    pgPrev.style.display = '';
+                }
+            };
+
+            this.prev = function() {
+                if (this.currentPage > 1) {
+                    this.showPage(this.currentPage - 1);
+                }
+            };
+
+            this.next = function() {
+                if (this.currentPage < this.pages) {
+                    this.showPage(this.currentPage + 1);
+                }
+            };
+
+            this.init = function() {
+                let rows = document.getElementById(tableName).rows;
+                let records = (rows.length - 1);
+
+                this.pages = Math.ceil(records / itemsPerPage);
+                this.inited = true;
+            };
+
+            this.showPageNav = function(pagerName, positionId) {
+                if (!this.inited) {
+                    // Not initialized
+                    return;
+                }
+
+                let element = document.getElementById(positionId),
+                    pagerHtml = '<span onclick="' + pagerName + '.prev();" class="pg-normal pg-prev">&#171;</span>';
+
+                for (let page = 1; page <= this.pages; page++) {
+                    pagerHtml += '<span id="pg' + page + '" class="pg-normal pg-next" onclick="' + pagerName + '.showPage(' + page + ');">' + page + '</span>';
+                }
+
+                pagerHtml += '<span onclick="' + pagerName + '.next();" class="pg-normal">&#187;</span>';
+
+                element.innerHTML = pagerHtml;
+            };
+        }
+
+
+
+        //
+        let pager = new Pager('pager', 5);
+
+        pager.init();
+        pager.showPageNav('pager', 'pageNavPosition');
+        pager.showPage(1);
+    </script>
+    <!-- table pagination done-->
     <script>
         var user = sessionStorage.getItem('UserName');
 
